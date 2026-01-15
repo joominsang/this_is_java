8f6a880431c34124c1b563fcd08e3f6181765840
joominsang <joominsang54@gmail.com>
2026-01-15 11:22:24 +0900

    example

--- chapter01/src/Main.java
+++ chapter01/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World! ");
+        for(String item : args) {
+            System.out.println(item);
+        }
     }
 }
