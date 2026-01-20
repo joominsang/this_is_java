2d7f11c79b643e33b7e84af61885b9d068aa30dc
joominsang <joominsang54@gmail.com>
2026-01-20 14:22:32 +0900

    chapter05

---
+++ chapter05/src/main/java/com/mjc813/Main.java
@@ -0,0 +1,41 @@
+package com.mjc813;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        chapter05 c01 = new chapter05();
+        c01.chp01();
+
+        chapter05 c02 = new chapter05();
+        c02.chp02();
+
+        chapter05 c03 = new chapter05();
+        c03.chp03();
+
+        chapter05 c04 = new chapter05();
+        c04.chp04();;
+
+        chapter05 c05 = new chapter05();
+        c05.chp05();
+
+        chapter05 c06 = new chapter05();
+        c06.chp06();
+
+        chapter05 c07 = new chapter05();
+        c07.chp07();
+
+        chapter05 c08 = new chapter05();
+        c08.chp08();
+
+        chapter05 c09 = new chapter05();
+        c09.chp09();
+
+        chapter05 c10 = new chapter05();
+        c10.chp10();
+
+        chapter05 c11 = new chapter05();
+        c11.chp11();
+    }
+}
