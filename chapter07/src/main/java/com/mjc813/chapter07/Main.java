f955664b23606a473a7eac4efcb3178b5f638c8a
joominsang <joominsang54@gmail.com>
2026-01-27 11:40:44 +0900

    chapter07 1번 예제

---
+++ chapter07/src/main/java/com/mjc813/chapter07/Main.java
@@ -0,0 +1,10 @@
+package com.mjc813.chapter07;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Chapter07 c01 = new Chapter07();
+
+        c01.SmartPhoneExample();
+    }
+}
