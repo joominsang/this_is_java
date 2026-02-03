673417f7473d8c3ac46de8634343d9100368ffe7
joominsang <joominsang54@gmail.com>
2026-02-03 14:00:43 +0900

    테스트

--- chapter10/maven_use_module/gson_module/src/main/java/com/mjc813/TransJson.java
+++ chapter10/maven_use_module/gson_module/src/main/java/com/mjc813/TransJson.java
@@ -5,17 +5,17 @@ import com.google.gson.Gson;
 import java.lang.reflect.Type;
 
 public class TransJson {
-	private final Gson gson;
+    private final Gson gson;
 
-	public TransJson() {
-		this.gson = new Gson();
-	}
+    public TransJson() {
+        this.gson = new Gson();
+    }
 
-	public String toJsonStringFromObject(Object obj) {
-		return this.gson.toJson(obj);
-	}
+    public String toJsonStringFromObject(Object obj) {
+        return this.gson.toJson(obj);
+    }
 
-	public Object toObjectFromJsonString(String str, Type type) {
-		return this.gson.fromJson(str, type);
-	}
+    public Object toObjectFromJsonString(String str, Type type) {
+            return this.gson.fromJson(str, type);
+    }
 }
